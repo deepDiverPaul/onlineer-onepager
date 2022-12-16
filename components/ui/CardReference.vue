542e4828c65a65d598936e8e96534c88c077a5f5
Paul Spenke <info@onlineer.de>
2022-12-16 11:31:14 +0800

    update images

--- components/ui/CardReference.vue
+++ components/ui/CardReference.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="card w-80 bg-base-100 shadow-xl hover:scale-100 scale-95 transition-all duration-500 border border-gray-200">
-    <nuxt-img format="webp" :src="`/assets/references/${item.identifier}.png`" :alt="item.title" class="rounded-2xl" width="604" loading="lazy" />
+    <img
+        :src="`/assets/references/${item.identifier}.png`"
+        :alt="item.title"
+        class="rounded-2xl"
+        width="604"
+        loading="lazy" />
     <div class="card-body">
       <h2 class="card-title">
         <a :href="item.url" :title="item.title">{{item.title}}&nbsp;<icon name="icon-park-outline:efferent-four" /></a>
